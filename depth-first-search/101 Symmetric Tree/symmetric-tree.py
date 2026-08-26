0b22501eed29c530c141ec027fe823ce1d876cb5
Tyree Franklin Jr. <tyree.franklinjr@gmail.com>
2026-08-25 19:34:50 -0500

    Add isSymmetric function for binary tree symmetry check
    Implement depth-first search to check if a binary tree is symmetric.

---
+++ depth-first-search/101 Symmetric Tree/symmetric-tree.py
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+
+bool dfs(struct TreeNode* left, struct TreeNode* right) {
+
+    /*
+    Depth-first search structure that utilizes recurisve
+    pattern to ultimately check for mirrors in the binary
+    tree. Returns the appropriate response per the prompt
+    */
+    
+    if (!left && !right) {return true;}
+    if (!left || !right) {return false;}
+    if (left->val != right->val) {return false;}
+
+    return (dfs(left->left, right->right) && dfs(left->right, right->left));
+}
+
+bool isSymmetric(struct TreeNode* root) {
+    
+    return dfs(root->left, root->right);
+
+}
+
+// Time Complexity -> O(N) as n is the number of elements in the root
+// Space Complexity -> O(1) as auxillary space remains constant
